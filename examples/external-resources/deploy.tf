11fd294f76eb88843b127def0bfba49c70ccf1d6
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-01-21 10:48:05 -0500

    fix: makes sure resources creations are disabled on external-resource example

--- examples/external-resources/deploy.tf
+++ examples/external-resources/deploy.tf
@@ -51,6 +51,9 @@ module "example" {
     env = "test"
   }
 
-  availability_set_id  = azurerm_availability_set.example.id
-  network_interface_id = azurerm_network_interface.example.id
+
+  availability_set_id       = azurerm_availability_set.example.id
+  availability_set_enabled  = false
+  network_interface_id      = azurerm_network_interface.example.id
+  network_interface_enabled = false
 }
