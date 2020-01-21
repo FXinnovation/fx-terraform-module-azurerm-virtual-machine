8d79c7d04d70c93de19561cafb9c4313e81dc7ba
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-01-21 10:21:03 -0500

    test: updates default example variables

--- examples/default/deploy.tf
+++ examples/default/deploy.tf
@@ -10,6 +10,20 @@ resource "azurerm_resource_group" "example" {
   location = "West US"
 }
 
+resource "azurerm_virtual_network" "example" {
+  name                = "tftest${random_string.this.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "tftest${random_string.this.result}"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefix       = "10.0.0.0/24"
+}
+
 module "example" {
   source = "../.."
 
@@ -21,6 +35,16 @@ module "example" {
 
   availability_set_name = "tftest${random_string.this.result}"
   availability_set_tags = {
+    test = "tftest${random_string.this.result}"
+  }
+
+  network_interface_enabled                       = true
+  network_interface_name                          = "tftest${random_string.this.result}"
+  network_interface_enable_ip_forwarding          = true
+  network_interface_enable_accelerated_networking = true
+  network_interface_ip_configuration_name         = "tftest${random_string.this.result}"
+  network_interface_ip_configuration_subnet_id    = azurerm_subnet.example.id
+  network_interface_tags = {
     test = "tftest${random_string.this.result}"
   }
 }
