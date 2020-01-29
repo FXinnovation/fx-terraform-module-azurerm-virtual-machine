6820a29b23cd1e2663bd32f14c798a875da7aa95
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-01-29 08:45:31 -0500

    test: temp moves example

--- external-resources/deploy.tf
+++ external-resources/deploy.tf
@@ -24,8 +24,28 @@ resource "azurerm_subnet" "example" {
   address_prefix       = "10.0.0.0/24"
 }
 
+resource "azurerm_availability_set" "example" {
+  name = "tftest${random_string.this.result}"
+
+  managed             = true
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "tftest${random_string.this.result}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "tftest${random_string.this.result}"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 module "example" {
-  source = "../.."
+  source = ".."
 
   resource_group_location = azurerm_resource_group.example.location
   resource_group_name     = azurerm_resource_group.example.name
@@ -33,19 +53,11 @@ module "example" {
     env = "test"
   }
 
-  availability_set_name = "tftest${random_string.this.result}"
-  availability_set_tags = {
-    test = "tftest${random_string.this.result}"
-  }
-
-  network_interface_enabled                    = true
-  network_interface_name                       = "tftest${random_string.this.result}"
-  network_interface_internal_dns_name_label    = "tftest${random_string.this.result}"
-  network_interface_ip_configuration_name      = "tftest${random_string.this.result}"
-  network_interface_ip_configuration_subnet_id = azurerm_subnet.example.id
-  network_interface_tags = {
-    test = "tftest${random_string.this.result}"
-  }
+  availability_set_name    = azurerm_availability_set.example.name
+  availability_set_enabled = true
+  availability_set_exists  = true
+  network_interface_names  = [azurerm_network_interface.example.name]
+  network_interface_exists = true
 
   os_profile_linux_config_ssh_keys = [{
     key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDD3gEe3zm4Z5AZtAD1qhD6f5hyg6qMBQA8SuMAVtAP8q8k/kFu/oCU6DUMUBO83SQIXBnEniBs2EMl8xUMXShrmYqHZE6bZZeBVg2y8Kr2ReCCSMPH5TDbPTWrGJR7x0SIBXgsjctOazCyMBB98lMgcK++P0PQnqGSvRj7iZbiyN2KNaXE1ukZ4USGeTWxoh9NFVilIt5R0pI5CECSLajKgXJMUl3QWc5bHL8fSpvHqoRfItiPEmpm5pSQb519jkdT7ohnhSwIA8qBo6sAnfrRH0ydLT3swglyn44FDs4hCSSK1Hu4n1vYMBWgzGyfxWJlVV483MJYduxamMGIpyjgLCRcQ7sIwWnkSepKpj6okEN+0D9JM/64uk5p0oZ1bBQ3UU/D1XDxOHkyOobFiGUn2GSnKs3CdDhLbKobjK2RN6Qs/mqJ2Ux8eqQr4n76X/4xHuuqtJMc/OyfOKTRE7BZ7MhBP5r6btMks2GEATye34qiHwH7YNy1/no2ynW8RI8= test@tests"
