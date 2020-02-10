a1876dd51292c3a8c39595bd72069fb51223ab35
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-02-10 18:36:50 -0500

    fix: makes output with zipmap conditionnal

--- outputs.tf
+++ outputs.tf
@@ -11,19 +11,19 @@ output "availability_set_id" {
 ###
 
 output "network_interface_ids" {
-  value = zipmap(azurerm_virtual_machine.this.*.id, chunklist(compact(concat(azurerm_network_interface.this.*.id, [""])), var.network_interface_count))
+  value = var.network_interface_count > 0 ? zipmap(azurerm_virtual_machine.this.*.id, chunklist(compact(concat(azurerm_network_interface.this.*.id, [""])), var.network_interface_count)) : {}
 }
 
 output "network_interface_mac_addresses" {
-  value = zipmap(azurerm_virtual_machine.this.*.id, chunklist(compact(concat(azurerm_network_interface.this.*.mac_address, [""])), var.network_interface_count))
+  value = var.network_interface_count > 0 ? zipmap(azurerm_virtual_machine.this.*.id, chunklist(compact(concat(azurerm_network_interface.this.*.mac_address, [""])), var.network_interface_count)) : {}
 }
 
 output "network_interface_private_ip_addresses" {
-  value = zipmap(azurerm_virtual_machine.this.*.id, chunklist(compact(concat(azurerm_network_interface.this.*.private_ip_address, [""])), var.network_interface_count))
+  value = var.network_interface_count > 0 ? zipmap(azurerm_virtual_machine.this.*.id, chunklist(compact(concat(azurerm_network_interface.this.*.private_ip_address, [""])), var.network_interface_count)) : {}
 }
 
 output "network_interface_virtual_machine_ids" {
-  value = zipmap(azurerm_virtual_machine.this.*.id, chunklist(compact(concat(azurerm_network_interface.this.*.virtual_machine_id, [""])), var.network_interface_count))
+  value = var.network_interface_count > 0 ? zipmap(azurerm_virtual_machine.this.*.id, chunklist(compact(concat(azurerm_network_interface.this.*.virtual_machine_id, [""])), var.network_interface_count)) : {}
 }
 
 ###
@@ -43,5 +43,5 @@ output "identities" {
 ###
 
 output "azurerm_managed_disk_ids" {
-  value = zipmap(azurerm_virtual_machine.this.*.id, chunklist(compact(concat(azurerm_managed_disk.this.*.id, [""])), var.managed_disk_count))
+  value = var.managed_disk_count > 0 ? zipmap(azurerm_virtual_machine.this.*.id, chunklist(compact(concat(azurerm_managed_disk.this.*.id, [""])), var.managed_disk_count)) : {}
 }
