bd7306b2fea2ba38b43b71692dacb9bd8eae9486
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-02-17 14:06:48 -0500

    fix: makes sure external disks have different names

--- examples/default/deploy.tf
+++ examples/default/deploy.tf
@@ -112,7 +112,7 @@ module "example" {
   ]
 
   managed_disk_count                   = 1
-  managed_disk_names                   = ["tftest${random_string.this.result}"]
+  managed_disk_names                   = ["tftest${random_string.this.result}ext"]
   managed_disk_source_vault_id         = azurerm_key_vault.example.id
   managed_disk_source_vault_uri        = azurerm_key_vault.example.vault_uri
   managed_disk_key_encryption_key_urls = [azurerm_key_vault_key.example.id]
